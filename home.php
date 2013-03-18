b2eaf805a06648e58134160d9f49f81f72a02888
Fabrizio <fabrizio.loreti@gmail.com>
2013-03-18 12:46:12 +0100

    prima bozza menu di navigazione

---
+++ home.php
@@ -0,0 +1,34 @@
+<?php
+
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+   "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+	<head>
+		<title>Sdei Mobili</title>
+		<link type="text/css" rel="stylesheet" href="css/default.css">
+	</head>
+	<body>
+		<div class="main_container">
+			<div class="site_container">
+				<div id="header" style="height: 300px;">
+				
+				</div>
+				<div id="navcontainer">
+					<ul id="navlist">
+						<li><a href="#">Home</a></li>
+						<li><a href="#">Products</a></li>
+						<li><a href="#">Services</a></li>
+						<li><a href="#">About</a></li>
+						<li><a href="#">Contact</a></li>
+						<li><a href="#" style="background-image: none;">Private Area</a></li>
+					</ul>
+					</div>
+				<div id="data">
+				</div>
+				<div id="footer">
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
